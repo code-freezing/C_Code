98e0520d50e05085f93629d62f813c3655743066
code-freezing <3102637943@qq.com>
2024-11-14 22:25:15 +0800

    24程设I-周7-课后4

---
+++ 001/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+long long cow(long long n) {
+	if (n % 3 == 0 && n / 3 >= 2) {
+        return 3*cow(n / 3);
+    }
+	else if ((n - 2) % 3 == 0 && (n - 2) / 3 >= 2) {
+        return 3*cow((n - 2) / 3) + 1;
+    }
+	else {
+        return 1;
+    }
+}
+
+int main() {
+	long long n;
+
+	scanf("%lld", &n);
+
+	printf("%lld\n", cow(n));
+	
+	return 0;
+}
